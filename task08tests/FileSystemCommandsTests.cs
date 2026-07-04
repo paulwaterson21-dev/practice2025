5a48875fc20fed3d95e9d67111a5f6a3c0505e01
paulwaterson21-dev <paulwaterson21@gmail.com>
2026-07-04 16:11:42 +0600

    выполнил задание task08: динамические бибблиотеки

---
+++ task08tests/FileSystemCommandsTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.IO;
+using Xunit;
+using task08;
+
+namespace task08tests
+{
+    public class FileSystemCommandsTests
+    {
+        [Fact]
+        public void DirectorySizeCommand_ShouldCalculateSize()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirSize");
+            if (Directory.Exists(testDir)) Directory.Delete(testDir, true);
+            Directory.CreateDirectory(testDir);
+
+            File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+            File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+            var command = new DirectorySizeCommand(testDir);
+            command.Execute();
+
+            Directory.Delete(testDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_ShouldFindMatchingFiles()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDirFind");
+            if (Directory.Exists(testDir)) Directory.Delete(testDir, true);
+            Directory.CreateDirectory(testDir);
+
+            File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+            File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+            var command = new FindFilesCommand(testDir, "*.txt");
+            command.Execute();
+
+            Directory.Delete(testDir, true);
+        }
+    }
+}
