ed4bd0b4ef7285d1e7b890c07c6e060f7f4eba19
paulwaterson21-dev <paulwaterson21@gmail.com>
2026-07-06 14:12:23 +0600

    фикс, добавлен конструктор по умолчанию и метод add для xunit тестов

--- task03/CustomCollection.cs
+++ task03/CustomCollection.cs
@@ -1,40 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+
 namespace task03
-{ 
-public class CustomCollection<T> : IEnumerable<T>
 {
-    private readonly List<T> _items;
-
-
-    public CustomCollection(IEnumerable<T> source)
+    public class CustomCollection<T> : IEnumerable<T>
     {
-        if (source == null)
+        private readonly List<T> _items;
+
+
+        public CustomCollection()
         {
-            throw new ArgumentNullException(nameof(source), "Входная коллекция не может быть null.");
+            _items = new List<T>();
         }
 
-        _items=new List<T>(source);
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        return _items.GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-
-    public IEnumerable<T> GetReverseEnumerator()
-    {
-        for (int i = _items.Count - 1; i>=0; i--)
+        public CustomCollection(IEnumerable<T> source)
         {
-            yield return _items[i];
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source), "Входная коллекция не может быть null.");
+            }
+
+            _items = new List<T>(source);
+        }
+
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<T> GetReverseEnumerator()
+        {
+            for (int i = _items.Count - 1; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
         }
     }
-}
 }
