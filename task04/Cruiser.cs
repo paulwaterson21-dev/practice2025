ca192a60ff84db6ff463ac32f7123cddba049dd3
paulwaterson21-dev <paulwaterson21@gmail.com>
2026-07-06 13:26:41 +0600

    фикс, убрал Console.WriteLine в реализации, почистил лишние файлы

--- task04/Cruiser.cs
+++ task04/Cruiser.cs
@@ -9,18 +9,17 @@ namespace task04
 
         public void MoveForward()
         {
-            Console.WriteLine("Крейсер движется вперед.");
+
         }
 
         public void Rotate(int angle)
         {
-            // Логика поворота
-            Console.WriteLine($"крейсер поворачивается на {angle} градусов.");
+
         }
 
         public void Fire()
         {
-            Console.WriteLine($"крейсер стреляет,урон: {FirePower}");
+
         }
     }
 }
--- task04/Fighter.cs
+++ task04/Fighter.cs
@@ -9,17 +9,17 @@ namespace task04
 
         public void MoveForward()
         {
-            Console.WriteLine("истребитель движется вперед.");
+
         }
 
         public void Rotate(int angle)
         {
-            Console.WriteLine($"истребитель поворачивается на {angle} градусов.");
+
         }
 
         public void Fire()
         {
-            Console.WriteLine($"истребитель стреляет,урон: {FirePower}");
+
         }
     }
 }
