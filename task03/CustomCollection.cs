da029920c2e54673edbdacc18c6c4a3615715751
paulwaterson21-dev <paulwaterson21@gmail.com>
2026-07-06 13:21:36 +0600

    фикс, добавлена валидация на null и удалены папки сборки

--- task03/CustomCollection.cs
+++ task03/CustomCollection.cs
@@ -1,37 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace task03
+public class CustomCollection<T> : IEnumerable<T>
 {
-    public class CustomCollection<T> : IEnumerable<T>
+    private readonly List<T> _items;
+
+
+    public CustomCollection(IEnumerable<T> source)
     {
-        private readonly List<T> _items = new();
-        public void Add(T item) => _items.Add(item);
-        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public IEnumerable<T> GetReverseEnumerator()
+        if (source == null)
         {
-            for (int i = _items.Count - 1; i >=0;i--)
-            {
-                yield return _items[i];
-            }
+            throw new ArgumentNullException(nameof(source), "Входная коллекция не может быть null.");
         }
-        
-        public static IEnumerable<int> GenerateSequence(int start, int count)
+
+        _items=new List<T>(source);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        for (int i = _items.Count - 1; i>=0; i--)
         {
-            for (int i=0; i < count; i++)
-            {
-                yield return start + i;
-            }
-        }
-        
-        public IEnumerable<T> FilterAndSort(Func<T, bool> predicate,Func<T,IComparable> keySelector)
-        {
-            return _items.Where(predicate).OrderBy(keySelector);
+            yield return _items[i];
         }
     }
 }
