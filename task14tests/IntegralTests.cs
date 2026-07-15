dfdb68b736ee35b6dc31df74298f76278fa77359
paulwaterson21-dev <paulwaterson21@gmail.com>
2026-07-15 12:27:59 +0600

    task14 выполнено

---
+++ task14tests/IntegralTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using task14;
+
+namespace task14tests
+{
+    public class IntegralTests
+    {
+        [Fact]
+        public void Test_Solve_LinearFunction()
+        {
+            Func<double, double> X = (double x) => x;
+            Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 4);
+        }
+
+        [Fact]
+        public void Test_Solve_SinFunction()
+        {
+            Func<double, double> SIN = (double x) => Math.Sin(x);
+            Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 4);
+        }
+
+        [Fact]
+        public void Test_Solve_LinearFunction_PositiveRange()
+        {
+            Func<double, double> X = (double x) => x;
+            Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 4);
+        }
+    }
+}
